e18759d74fd3decfde2001c58d601afcba065446
Martin Sonnenholzer <martin.sonnenholzer@idealo.de>
2018-05-25 11:55:12 +0200

    example Please enter the commit message for your changes. Lines starting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EntryComponentComponent } from './submodule/entry-component/entry-component.component';
+import { DefaultRootComponent } from './default-root/default-root.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DefaultRootComponent
+  },
+  {
+    path: 'submodule',
+    component: EntryComponentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubmoduleModule } from './submodule/submodule.module';
+import { DefaultRootComponent } from './default-root/default-root.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultRootComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SubmoduleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
